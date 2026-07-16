a3c4b4ebc1436d796cc02ff096eb0e1eef565ffa
Kripa N <nkripa00@gmail.com>
2026-07-16 17:04:47 -0500

    Initial commit, data loaded

---
+++ README.md
@@ -0,0 +1,6 @@
+# Facial Emotion Recognition using CNN
+
+
+## Dataset
+Download the FER-2013 Dataset from [https://www.kaggle.com/datasets/msambare/fer2013](https://www.kaggle.com/datasets/msambare/fer2013)
+and place it in the data/ directory
---
+++ src/load_data.py
@@ -0,0 +1,56 @@
+import os
+from PIL import Image                   #Pillow
+
+def load_images(data_path):             #data_path from main()
+
+    images = []                        #pixels
+    labels = []                        #emotions
+
+    for emotion in os.listdir(data_path):                   #get names of all emotion folders (angry, sad, etc.) 
+        print(f"loading {emotion}...")
+
+        emotion_path = os.path.join(data_path, emotion)     #join data/train/ + happy/ for each emotion
+        
+        if os.path.isdir(emotion_path):
+            for image_name in os.listdir(emotion_path):
+                image_path = os.path.join(emotion_path, image_name)
+
+                try:
+                    image = Image.open(image_path).convert("L")     #open the image as grayscale
+                except Exception as e:
+                    continue
+
+                images.append(image)
+                labels.append(emotion)
+
+                """
+                image = PIL.Image.Image object
+                images = list of PIL objects
+                """
+
+    return images, labels
+
+"""
+data_path:
+    received from main()
+    path to data/train/ or data/test/ 
+
+Workflow
+    loop through the train or test folders
+    read the name of immediate children and label them emotions
+    loop through every image in each emotion folder
+    (form path for each image)
+    open image in grayscale
+    store the image and its label
+    return two lists:
+        images -> image objects
+        labels -> emotion names
+"""
+
+#test
+if __name__ == "__main__":
+    images, labels = load_images("data/train")
+
+    print("Nummber of images: ", len(images))
+    print("First label:", labels[0])
+    print("First image size:", images[0].size)
